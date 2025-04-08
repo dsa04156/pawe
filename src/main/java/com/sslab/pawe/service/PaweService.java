4032f40dc56a2c1dd96b95184726921b6cc50509
정진욱 <dsa04156@naver.com>
2025-04-08 17:34:39 +0900

    feat: init

--- src/main/java/com/sslab/pawe/service/PaweService.java
+++ src/main/java/com/sslab/pawe/service/PaweService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaweService {
 
-    private final WasmExecutor wasmExecutor = new WasmExecutor("/pawe_wasm.wasm");
+    private final WasmExecutor wasmExecutor = new WasmExecutor("/wasm/pawe_wasm.wasm");
 
     public String handlePrompt(String input) {
         // WASM 모듈의 경로는 실제 Docker 환경에서의 파일 경로나 절대경로로 수정해야 합니다.
